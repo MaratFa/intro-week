a5f9da4cfee1b9e8e9935dea4774aea6a2b38c02
MaratFa <evolution99@rambler.ru>
2025-11-17 18:46:10 +0300

    edited

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { renderCards } from "./functions/render-cards.js";
 import { renderProgress } from "./functions/render-progress.js";
 import { content } from "./constants/constants.js";
-import { backdrop } from "./constants/constants.js";
+import { backdrop } from "../constants/constants.js";
 import { openCard } from "./functions/open-card.js";
 import { closeModal } from "./functions/close-modal.js";
 
