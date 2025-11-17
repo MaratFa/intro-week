c36604778cbcf3a4306a35d413c4d6428e38cc9b
MaratFa <evolution99@rambler.ru>
2025-11-17 18:46:33 +0300

    edited

--- constants/constants.js
+++ constants/constants.js
@@ -2,3 +2,4 @@ export const modal = document.querySelector("#modal");
 export const content = document.querySelector("#content");
 export const backdrop = document.querySelector("#backdrop");
 export const progress = document.querySelector("#progress");
+export const APP_TTTLE = document.title;
--- data/technologies.js
+++ data/technologies.js
@@ -1,7 +1,7 @@
 export const technologies = [
   {
     id: 1,
-    name: "HTML",
+    title: "HTML",
     description:
       "HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.",
     type: "Frontend",
@@ -10,7 +10,7 @@ export const technologies = [
   },
   {
     id: 2,
-    name: "CSS",
+    title: "CSS",
     description:
       "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
     type: "Frontend",
@@ -19,7 +19,7 @@ export const technologies = [
   },
   {
     id: 3,
-    name: "JavaScript",
+    title: "JavaScript",
     description:
       "JavaScript, often abbreviated JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.",
     type: "Frontend",
@@ -28,7 +28,7 @@ export const technologies = [
   },
   {
     id: 4,
-    name: "React",
+    title: "React",
     description:
       "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.",
     type: "Frontend",
@@ -37,7 +37,7 @@ export const technologies = [
   },
   {
     id: 5,
-    name: "Node.js",
+    title: "Node.js",
     description:
       "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.",
     type: "Backend",
