734fb2aa922cb5d6e19fd084b1a1d0914abb5e82
MaratFa <evolution99@rambler.ru>
2025-11-18 08:56:52 +0300

    edited

--- data/technologies.js
+++ data/technologies.js
@@ -15,7 +15,7 @@ export const technologies = [
       "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.",
     type: "Frontend",
     image: "https://img.icons8.com/color/48/000000/css3.png",
-    done: false,
+    done: true,
   },
   {
     id: 3,
