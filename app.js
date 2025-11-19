c61e9103a3132106faae9f46e277463975a99ec2
MaratFa <evolution99@rambler.ru>
2025-11-20 01:21:28 +0300

    edited

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { openCard } from "./functions/open-card.js";
 import { closeModal } from "./functions/close-modal.js";
 import { toggleTech } from "./functions/toggle-tech.js";
 import { createTech } from "./functions/create-tech.js";
-import { init } from "./init.js";
+import { init } from "./functions/init.js";
 
 content.addEventListener("click", openCard);
 backdrop.addEventListener("click", closeModal);
