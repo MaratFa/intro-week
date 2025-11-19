f606034f796a5f342f2fed2d0c3cbfea617012ca
MaratFa <evolution99@rambler.ru>
2025-11-19 17:11:07 +0300

    edited

--- constants/constants.js
+++ constants/constants.js
@@ -3,3 +3,4 @@ export const content = document.querySelector("#content");
 export const backdrop = document.querySelector("#backdrop");
 export const progress = document.querySelector("#progress");
 export const APP_TTTLE = document.title;
+export const form = document.querySelector("#form");
