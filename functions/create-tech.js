760ebeb8476fbaaebe7803f0330994a7a384b824
MaratFa <evolution99@rambler.ru>
2025-11-20 01:21:31 +0300

    edited

--- functions/create-tech.js
+++ functions/create-tech.js
@@ -1,3 +1,9 @@
 export function createTech(event) {
   event.preventDefault();
+
+  const title = event.target.title;
+  const description = event.target.description;
+  console.log(title);
+  console.log(description);
+  
 }
