00df4a615f4b9898b847aca69eb8248efa51158c
MaratFa <evolution99@rambler.ru>
2025-11-19 14:45:08 +0300

    edited

--- functions/toggle-tech.js
+++ functions/toggle-tech.js
@@ -1,12 +1,12 @@
+import { technologies } from "../data/technologies.js";
+import { renderCards } from "./render-cards.js";
+import { renderProgress } from "./render-progress.js";
+
 export function toggleTech(event) {
-  console.log(event.target.dataset.id);
+  const id = Number(event.target.dataset.id);
+  const tech = technologies.find((e) => e.id === id);
+  tech.done = event.target.checked;
 
-  
-
-
-
-
-
-
-  
+  renderCards();
+  renderProgress();
 }
