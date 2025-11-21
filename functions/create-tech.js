95cac015d74a88b0fb083b0f121b0c21bdf33169
MaratFa <evolution99@rambler.ru>
2025-11-21 10:34:34 +0300

    edited

--- functions/create-tech.js
+++ functions/create-tech.js
@@ -1,9 +1,33 @@
+import { technologies } from "../data/technologies.js";
+import { init } from "./init.js";
+import { isInValid } from "./is-invalid.js";
+import { saveState } from "./save-state.js";
+
 export function createTech(event) {
   event.preventDefault();
 
-  const title = event.target.title;
-  const description = event.target.description;
-  console.log(title);
-  console.log(description);
-  
+  const { title, description } = event.target;
+
+  if (isInValid(title, description)) {
+    if (!title.value) title.classList.add("invalid");
+    if (!description.value) description.classList.add("invalid");
+    setTimeout(() => {
+      title.classList.remove("invalid");
+      description.classList.remove("invalid");
+    }, 2000);
+    return;
+  }
+
+  const newTech = {
+    id: technologies.length + 1,
+    title: title.value,
+    description: description.value,
+    done: false,
+  };
+
+  technologies.push(newTech);
+  title.value = "";
+  description.value = "";
+  saveState();
+  init();
 }
